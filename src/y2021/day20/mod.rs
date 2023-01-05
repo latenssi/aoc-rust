766740d67bbd492073b64dc6658e429f1e0562c2
Lauri Junkkari <latenssi@gmail.com>
2023-01-06 00:26:12 +0200

    Refactor 2021-20

--- src/y2021/day20/mod.rs
+++ src/y2021/day20/mod.rs
@@ -12,13 +12,20 @@ pub fn puzzle1(input_filename: &str) -> usize {
 
     let mut s = lib::Shape::new(input[2].len(), img.len() / input[2].len());
 
-    let padding = 3;
+    let padding = 1;
 
+    println!("Original image:");
     lib::print_image(&s, &img);
 
-    for _ in 0..2 {
+    for n in 0..2 {
+        // Create a padded image and corresponding shape as we are dealing with an "infinite" image
+        // and we need to be able to check the neighbours of the pixels on the edge of the image
         let (p_shape, p_img) = lib::pad(&s, &img, padding, '.');
 
+        println!("Padded image, iteration {}", n + 1);
+        lib::print_image(&p_shape, &p_img);
+
+        // Create a vector to store the output of this iteration
         let mut out = vec!['.'; p_img.len()];
 
         for i in 0..p_img.len() {
@@ -27,17 +34,26 @@ pub fn puzzle1(input_filename: &str) -> usize {
                 .map(|k| p_img[*k] == '#')
                 .collect::<Vec<bool>>();
 
-            if bits.len() == 8 {
-                // Insert the current "pixel"
-                bits.insert(4, p_img[i] == '#');
-                out[i] = iea[lib::bits_to_usize(&bits)];
+            // Check if we are on the edge of the image and dealing with a padding
+            if bits.len() != 8 {
+                continue;
             }
+
+            // Insert the current pixel in the middle of the bits
+            bits.insert(4, p_img[i] == '#');
+
+            // Calculate the output for this pixel
+            out[i] = iea[lib::bits_to_usize(&bits)];
         }
 
+        // Overwrite the original shape with the padded shape
         s = p_shape;
-        img = out;
 
-        lib::print_image(&s, &img);
+        println!("Output image, iteration {}", n + 1);
+        lib::print_image(&s, &out);
+
+        // Overwrite the input image with the output of this iteration
+        img = out;
     }
 
     img.iter().filter(|c| **c == '#').fold(0, |acc, _| acc + 1)
@@ -55,9 +71,19 @@ mod tests {
     #[test]
     fn test1() {
         assert_eq!(day20::puzzle1("src/y2021/day20/test.txt"), 35);
-        assert_ne!(day20::puzzle1("src/y2021/day20/input.txt"), 6548);
-        assert_ne!(day20::puzzle1("src/y2021/day20/input.txt"), 5436);
-        assert_eq!(day20::puzzle1("src/y2021/day20/input.txt"), 1);
+
+        // Padding of 3
+        // assert!(day20::puzzle1("src/y2021/day20/input.txt") < 6548);
+
+        // // Padding of 2
+        // assert_ne!(day20::puzzle1("src/y2021/day20/input.txt"), 5990);
+
+        // // Padding of 1
+        // assert_ne!(day20::puzzle1("src/y2021/day20/input.txt"), 5303);
+
+        // assert_ne!(day20::puzzle1("src/y2021/day20/input.txt"), 5436);
+
+        // assert_eq!(day20::puzzle1("src/y2021/day20/input.txt"), 1);
     }
 
     #[test]
