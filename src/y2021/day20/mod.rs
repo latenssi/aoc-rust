b5c9a3ac9b1e33c980a3d9f528bd7f9bb730feb7
Lauri Junkkari <latenssi@gmail.com>
2023-01-07 22:38:11 +0200

    2021 day 20, part 2

--- src/y2021/day20/mod.rs
+++ src/y2021/day20/mod.rs
@@ -1,6 +1,6 @@
 use crate::lib;
 
-pub fn puzzle1(input_filename: &str) -> usize {
+pub fn puzzle(input_filename: &str, iterations: usize) -> usize {
     let input = lib::read_lines(input_filename);
     let iea = &input[0].chars().collect::<Vec<char>>();
     let mut img = input
@@ -10,55 +10,57 @@ pub fn puzzle1(input_filename: &str) -> usize {
         .flat_map(|s| s.chars().collect::<Vec<char>>())
         .collect::<Vec<char>>();
 
-    let mut s = lib::Shape::new(input[2].len(), img.len() / input[2].len());
+    let mut shape = lib::Shape::new(input[2].len(), img.len() / input[2].len());
 
-    for n in 0..2 {
-        let padder = match n {
-            // On first iteration, pad with '.'
-            0 => '.',
-            // On the second iteration, pad depends on what is in iea[0]
-            1 => iea[0],
-            _ => panic!("Invalid iteration"),
-        };
+    let mut padder = '.';
 
-        // Create a padded shape and image, pad by 1 pixel in all directions
-        let (p_shape, p_img) = lib::pad(&s, &img, 1, padder);
-
-        // Create a vector to store the output of this iterations
-        let mut out: Vec<char> = vec![];
-
-        for i in 0..p_img.len() {
-            // Get the neighbours of the current pixel and convert them to bools.
-            // 'neighbours_2' returns a vector of Option<usize>, of indexes of the neighbours.
-            // The Option is None when we are looking at a pixel outside the shape.
-            let mut bits = lib::neighbours_2(&p_shape, i, true)
-                .iter()
-                .map(|j| match j {
-                    Some(j) => p_img[*j] == '#',
-                    None => padder == '#',
-                })
-                .collect::<Vec<bool>>();
-
-            // Insert the current pixel in the middle of the bits
-            bits.insert(4, p_img[i] == '#');
-
-            // Calculate the output for this pixel
-            out.push(iea[lib::bits_to_usize(&bits)]);
-        }
-
-        // Overwrite the original shape with the padded shape
-        s = p_shape;
-
-        // Overwrite the input image with the output of this iteration
-        img = out;
+    for _ in 0..iterations {
+        enhance(iea, &mut padder, &mut img, &mut shape);
     }
 
     img.iter().filter(|c| **c == '#').fold(0, |acc, _| acc + 1)
 }
 
-pub fn puzzle2(input_filename: &str) -> isize {
-    let input = lib::read_lines(input_filename);
-    0
+fn enhance(iea: &Vec<char>, padder: &mut char, img: &mut Vec<char>, shape: &mut lib::Shape) {
+    let c_padder = *padder;
+
+    // Create a padded shape and image, pad by 1 pixel in all directions
+    let (p_shape, p_img) = lib::pad(shape, img, 1, c_padder);
+
+    // Create a vector to store the output of this iteration
+    let mut out: Vec<char> = vec![];
+
+    for i in 0..p_img.len() {
+        // Get the neighbours of the current pixel and convert them to bools.
+        // 'neighbours_2' returns a vector of Option<usize>, of indexes of the neighbours.
+        // The Option is None when we are looking at a pixel outside the shape.
+        let mut bits = lib::neighbours_2(&p_shape, i, true)
+            .iter()
+            .map(|j| match j {
+                Some(j) => p_img[*j] == '#',
+                None => c_padder == '#',
+            })
+            .collect::<Vec<bool>>();
+
+        // Insert the current pixel in the middle of the bits
+        bits.insert(4, p_img[i] == '#');
+
+        // Calculate the output for this pixel
+        out.push(iea[lib::bits_to_usize(&bits)]);
+    }
+
+    // Overwrite the original shape with the padded shape
+    *shape = p_shape;
+
+    // Overwrite the input image with the output of this iteration
+    *img = out;
+
+    // Determine the next padding character
+    *padder = match c_padder {
+        '#' => iea[511],
+        '.' => iea[0],
+        _ => panic!("Invalid char"),
+    };
 }
 
 #[cfg(test)]
@@ -67,23 +69,23 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(day20::puzzle1("src/y2021/day20/test.txt"), 35);
+        assert_eq!(day20::puzzle("src/y2021/day20/test.txt", 2), 35);
 
-        assert!(day20::puzzle1("src/y2021/day20/input.txt") < 6548);
+        assert!(day20::puzzle("src/y2021/day20/input.txt", 2) < 6548);
 
-        assert_ne!(day20::puzzle1("src/y2021/day20/input.txt"), 5990);
-        assert_ne!(day20::puzzle1("src/y2021/day20/input.txt"), 5303);
-        assert_ne!(day20::puzzle1("src/y2021/day20/input.txt"), 5436);
-        assert_ne!(day20::puzzle1("src/y2021/day20/input.txt"), 5092);
-        assert_ne!(day20::puzzle1("src/y2021/day20/input.txt"), 5570);
-        assert_ne!(day20::puzzle1("src/y2021/day20/input.txt"), 6112);
+        assert_ne!(day20::puzzle("src/y2021/day20/input.txt", 2), 5990);
+        assert_ne!(day20::puzzle("src/y2021/day20/input.txt", 2), 5303);
+        assert_ne!(day20::puzzle("src/y2021/day20/input.txt", 2), 5436);
+        assert_ne!(day20::puzzle("src/y2021/day20/input.txt", 2), 5092);
+        assert_ne!(day20::puzzle("src/y2021/day20/input.txt", 2), 5570);
+        assert_ne!(day20::puzzle("src/y2021/day20/input.txt", 2), 6112);
 
-        assert_eq!(day20::puzzle1("src/y2021/day20/input.txt"), 5425);
+        assert_eq!(day20::puzzle("src/y2021/day20/input.txt", 2), 5425);
     }
 
     #[test]
     fn test2() {
-        assert_eq!(day20::puzzle2("src/y2021/day20/test.txt"), 1);
-        assert_eq!(day20::puzzle2("src/y2021/day20/input.txt"), 1);
+        assert_eq!(day20::puzzle("src/y2021/day20/test.txt", 50), 3351);
+        assert_eq!(day20::puzzle("src/y2021/day20/input.txt", 50), 14052);
     }
 }
